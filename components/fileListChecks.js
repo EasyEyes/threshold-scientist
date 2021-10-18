135231aa249d8fbc94de7d20da1e36017cadab28
Augustin Burchell <augustin.burchell@gmail.com>
2021-10-18 12:41:09 -0500

    Syntactic experiment file checks complete

--- components/fileListChecks.js
+++ components/fileListChecks.js
@@ -1,41 +1,20 @@
-export const validateFileList = (fileList) => {};
+import {
+  NO_CSV_FILE_FOUND,
+  TOO_MANY_CSV_FILES_FOUND,
+} from "./errorMessages.js";
+
+export const validateFileList = (fileList) => {
+  const numberOfCsvFiles = fileList.filter(isCsvFile).length;
+  if (numberOfCsvFiles < 1) return NO_CSV_FILE_FOUND;
+  if (numberOfCsvFiles > 1) return TOO_MANY_CSV_FILES_FOUND;
+};
 
 /**
- * Determines whether a single .csv file (to be used as the experiment specification table) is present;
- * returns it, or an error explaining why it couldn't be identified, e.g. there were no candidates or it was ambiguous.
+ * Assuming there is only one csv file present, return it to be used as the experiment file
  * @assumes .csv files are only used to specify the 'experiment.csv' file, aka the experiment specification
  * @param {File[]} filesProvided Set of files that the user has provided
- * @returns {[Boolean, File, Object]} [isAnIdentifiableExperimentFilePresent, thatExperimentFileIfSo, anErrorExplainingIfNot]
+ * @returns {File}
  */
-const identifyExperimentFile = (filesProvided) => {
-  const isCsvFile = (file) => file.type == "text/csv";
-  // Find how many .csv files are provided
-  const numberOfCsvFiles = fileList.filter(isCsvFile).length;
-  if (numberOfCsvFiles < 1)
-    return [
-      false,
-      undefined,
-      {
-        name: "No CSV files provided.",
-        context: `Within identifyExperimentFile, give the files: ${filesProvided}`,
-        message:
-          "When looking for an experiment file, I couldn't even find one .csv file as a candidate.",
-        hint: "Make sure you provide a file with the '.csv' extension amongst your files -- this file will be used as your experiment specification.",
-      },
-    ];
-  if (numberOfCsvFiles > 1)
-    return [
-      false,
-      undefined,
-      {
-        name: "Multiple CSV files provided.",
-        context: `Within identifyExperimentFile, give the files: ${filesProvided}, I found the following .csv files: ${fileList.fileter(
-          isCsvFile
-        )}`,
-        message:
-          "When looking for an experiment file, I found more than one .csv file, and I don't know which one to pick!",
-        hint: "Make sure you provide a file with the '.csv' extension amongst your files -- this file will be used as your experiment specification.",
-      },
-    ];
-  return [true, fileList.filter(isCsvFile)[0], {}];
+const returnExperimentFile = (filesProvided) => {
+  return fileList.filter(isCsvFile)[0];
 };
