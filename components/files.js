9cf04431407716396b7cbbf3eb470aa083fb2900
Sagar R Pandav <sagar.rpandav@gmail.com>
2021-10-23 17:07:58 -0400

    fix undefined dialog box

--- components/files.js
+++ components/files.js
@@ -3,4 +3,19 @@
 */
 
 const _loadDir = "/threshold/threshold/";
-const _loadFiles = [];
+const _loadFiles = [
+  "LICENSE",
+  "README.md",
+  "css/consent.css",
+  "css/custom.css",
+  "css/instructions.css",
+  "css/showAlphabet.css",
+  "css/utils.css",
+  "index.html",
+  "js/threshold.min.js",
+  "js/threshold.min.js.LICENSE.txt",
+  "js/threshold.min.js.map",
+  "psychojs/LICENSE.md",
+  "psychojs/out/psychojs-2021.3.0.css",
+  "threshold.js",
+];
