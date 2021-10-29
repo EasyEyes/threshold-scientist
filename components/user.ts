ce60b7950a9a76266ef25e371fae87c3fe1acf9f
Shikhar Vaish <shikhar.vaish90@gmail.com>
2021-10-29 02:45:03 -0400

    switched to prod config

--- components/user.ts
+++ components/user.ts
@@ -82,7 +82,7 @@ export const populateUserInfo = async () => {
 export const redirectToOauth2 = () => {
   // TODO switch this for production
   //location.href = env.PRODUCTION;
-  location.href = env.DEVELOPMENT.GITLAB_REDIRECT_URL;
+  location.href = env.PRODUCTION.GITLAB_REDIRECT_URL;
 };
 
 export const redirectToPalvoliaActivation = async () => {
