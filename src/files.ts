6f449ceb766b82bfd68bed9d8f38bdc72e055de7
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2021-11-09 00:15:55 -0500

    update files

--- src/files.ts
+++ src/files.ts
@@ -6,11 +6,12 @@ export const _loadDir: string = "/threshold/threshold/";
 export const _loadFiles: string[] = [
   "LICENSE",
   "README.md",
-  "css/consent.css",
   "css/custom.css",
+  "css/forms.css",
   "css/instructions.css",
   "css/showAlphabet.css",
   "css/utils.css",
+  "css/widgets.css",
   "index.html",
   "js/threshold.min.js",
   "js/threshold.min.js.LICENSE.txt",
