cc2feb348d17ea6b0cee86686df009febb4b9d03
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2021-11-12 12:31:46 -0500

    update errors

--- src/errorMessages.ts
+++ src/errorMessages.ts
@@ -25,17 +25,17 @@ export const INCORRECT_PARAMETER_TYPE = (
   correctType: "integer" | "numerical" | "text" | "boolean" | "categorical",
   categories?: string[]
 ): EasyEyesError => {
-  const offendingsMessage = offendingValues.map(
-    (offending) => `\"${offending.value}\" [condition ${offending.block}]`
+  const offendingMessage = offendingValues.map(
+    (offending) => ` "${offending.value}" [condition ${offending.block}]`
   );
-  let message: string = `All values for the parameter \'${parameter}\' must be ${correctType} type.`;
+  let message: string = `All values for the parameter "${parameter}" must be "${correctType}" type.`;
   if (categories) {
-    message = message + ` Valid categories are: ${categories}.`;
+    message = message + ` Valid categories are: ${categories.join(", ")}.`;
   }
   return {
-    name: `Parameter \'${parameter}\' contains values of the wrong type.`,
+    name: `Parameter "${parameter}" contains values of the wrong type.`,
     message: message,
-    hint: `We're having trouble with the following values, try double checking them: ${offendingsMessage}`,
+    hint: `We're having trouble with the following values, try double checking them:${offendingMessage}.`,
     context: "preprocessor",
     kind: "error",
   };
@@ -47,9 +47,8 @@ export const EXPERIMENT_FILE_NOT_FOUND = (
   return {
     name: "Unable to identify experiment file",
     context: `preprocessor`,
-    message:
-      "Sorry, we weren't able to find an csv file, eg 'experiment.csv', in the files that you provided. This file is required, as it defines your entire experiment -- we can't make your movie without your don't provide a screenplay.",
-    hint: "Make sure you include exactly one '.csv' file among the files you upload. This file should be in row-major order, ie one row representing each parameter, and should follow the specification laid out in the EasyEyes Threshold Glossary.",
+    message: `Sorry, we weren't able to find an csv file, e.g., "experiment.csv", in the files that you provided. This file is required, as it defines your entire experiment -- we can't make your movie without your don't provide a screenplay.`,
+    hint: `Make sure you include exactly one ".csv" file among the files you upload. This file should be in row-major order, ie one row representing each parameter, and should follow the specification laid out in the EasyEyes Threshold Glossary.`,
     kind: "error",
   };
 };
@@ -58,7 +57,7 @@ export const NO_CSV_FILE_FOUND: EasyEyesError = {
   name: "No CSV files provided.",
   message:
     "When looking for an experiment file, we couldn't even find one .csv file as a candidate.",
-  hint: "Make sure you provide a file with the '.csv' extension amongst your files -- this file will be used as your experiment specification.",
+  hint: `Make sure you provide a file with the ".csv" extension amongst your files -- this file will be used as your experiment specification.`,
   context: "preprocessor",
   kind: "error",
 };
@@ -67,7 +66,7 @@ export const TOO_MANY_CSV_FILES_FOUND: EasyEyesError = {
   name: "Multiple CSV files provided.",
   message:
     "When looking for an experiment file, we found more than one .csv file, and we don't know which one to pick!",
-  hint: "Make sure you provide a file with the '.csv' extension amongst your files -- this file will be used as your experiment specification.",
+  hint: `Make sure you provide a file with the ".csv" extension amongst your files -- this file will be used as your experiment specification.`,
   context: "preprocessor",
   kind: "error",
 };
@@ -94,9 +93,9 @@ export const DUPLICATE_PARAMETER = (parameter: string): EasyEyesError => {
 // TODO create type to match report object structure
 export const UNRECOGNIZED_PARAMETER = (report: any): EasyEyesError => {
   return {
-    name: `Parameter \'${report.name}\' is unrecognized.`,
-    message: `Sorry, we couldn't recognize the parameter ${report.name}. The closest supported parameter is \'${report.closest[0]}\' -- is that what you meant?`,
-    hint: `Make sure that you are only including parameter which are supported, and remember that all parameters are case-sensitive. Double check the spelling of \'${report.name}\', if you're confident it ought to be supported. The other closest supported parameters found were ${report.closest[1]}, ${report.closest[2]}, and ${report.closest[3]}.`,
+    name: `Parameter "${report.name}" is unrecognized`,
+    message: `Sorry, we couldn't recognize the parameter ${report.name}. The closest supported parameter is "${report.closest[0]}" - is that what you meant?`,
+    hint: `Make sure that you are only including parameter which are supported, and remember that all parameters are case-sensitive. Double check the spelling of "${report.name}". If you're confident it ought to be supported. The other closest supported parameters found were ${report.closest[1]}, ${report.closest[2]}, and ${report.closest[3]}.`,
     context: "preprocessor",
     kind: "error",
   };
@@ -108,8 +107,8 @@ export const NOT_YET_SUPPORTED_PARAMETER = (
   /*    let glossaryKey = Object.keys(GLOSSARY).find(i => i == parameter);
     let glossaryObject = glossaryKey && GLOSSARY[glossaryKey];*/
   return {
-    name: `Parameter \'${parameter}\' is not yet supported`,
-    message: `Apologies from the EasyEyes team! The parameter \'${parameter}\' isn't supported yet. We hope to implement the parameter ${GLOSSARY[parameter]?.availability}.`,
+    name: `Parameter "${parameter}" is not yet supported`,
+    message: `Apologies from the EasyEyes team! The parameter "${parameter}" isn't supported yet. We hope to implement the parameter ${GLOSSARY[parameter]?.availability}.`,
     hint: "Unfortunately, you won't be able to use this parameter at this time. Please, try again later. If the parameter is important to you, we'd encourage you to reach out and email the EasyEyes team at easyeyes.team@gmail.com",
     context: "preprocessor",
     kind: "error",
