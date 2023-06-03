aa3fa847856b89546791a20c43e34087920a9456
ritikamotwani <ritika.motwani0712@gmail.com>
2023-06-02 23:13:25 -0700

    for threshold-scientist: Update the eligibility requirements for prolific

--- source/components/prolificIntegration.js
+++ source/components/prolificIntegration.js
@@ -72,6 +72,144 @@ const findProlificLocationEligibilityAttributes = (field) => {
   return result;
 };
 
+const buildEligibilityRequirements = (
+  whiteListParticipants,
+  user,
+  blockListParticipants
+) => {
+  const req = [
+    ...(user.currentExperiment && user.currentExperiment._online5LanguageFirst
+      ? [
+          {
+            id: null,
+            type: "SelectAnswer",
+            attributes: findProlificLanguageAttributes(
+              user.currentExperiment._online5LanguageFirst
+            ),
+            query: {
+              id: "54ac6ea9fdf99b2204feb899",
+              question: "What is your first language?",
+              description: "",
+              title: "First language",
+              help_text: "",
+              participant_help_text: "",
+              researcher_help_text: "",
+              is_new: false,
+              tags: ["onboarding-2", "core-5", "default_export_language"],
+            },
+          },
+        ]
+      : []),
+    ...(user.currentExperiment && user.currentExperiment._online5LanguageFluent
+      ? [
+          {
+            id: null,
+            type: "MultiSelectAnswer",
+            attributes: findProlificLanguageAttributes(
+              user.currentExperiment._online5LanguageFluent,
+              prolificLangType.FLUENT
+            ),
+            query: {
+              id: "58c6b44ea4dd0a4799361afc",
+              question: "Which of the following languages are you fluent in?",
+              description: "Select the languages that you are fluent in.",
+              title: "Fluent languages",
+              help_text:
+                "<b>Please note:</b> Participants who are fluent in any of the selected languages will be eligible for the study. For example, the study will recruit participants who are fluent in English or German, but not both.",
+              participant_help_text: "",
+              researcher_help_text: "",
+              is_new: false,
+              tags: ["rep_sample_language", "core-13"],
+            },
+          },
+        ]
+      : []),
+    ...(user.currentExperiment && user.currentExperiment._online4Location
+      ? [
+          {
+            id: null,
+            type: "SelectAnswer",
+            attributes: findProlificLocationEligibilityAttributes(
+              user.currentExperiment._online4Location
+            ),
+            query: {
+              id: "54bef0fafdf99b15608c504e",
+              question: "In what country do you currently reside?",
+              description: "",
+              title: "Current Country of Residence",
+              help_text:
+                "Please note that Prolific is currently only available for participants who live in OECD countries. <a href='https://researcher-help.prolific.co/hc/en-gb/articles/360009220833-Who-are-the-people-in-your-participant-pool' target='_blank'>Read more about this</a>",
+              participant_help_text: "",
+              researcher_help_text: "",
+              is_new: false,
+              tags: [
+                "rep_sample_country",
+                "core-7",
+                "default_export_country_of_residence",
+              ],
+            },
+          },
+        ]
+      : []),
+    ...(whiteListParticipants.length > 0
+      ? [
+          {
+            requirement_type: "Allow List",
+            query: {
+              question: null,
+              description:
+                "A comma separated list of Prolific IDs for users you want to participate in the study.",
+              title: "Custom Whitelist",
+              help_text:
+                "ONLY these participants will be eligible for this study. (i.e. longitudinal design). <a href='https://researcher-help.prolific.co/hc/en-gb/articles/360015365674-How-to-invite-specific-participants-to-your-study'>Read about how to invite specific participants to your study.</a>",
+              researcher_help_text:
+                "ONLY these participants will be eligible for this study. (i.e. longitudinal design). <a href='https://researcher-help.prolific.co/hc/en-gb/articles/360015365674-How-to-invite-specific-participants-to-your-study'>Read about how to invite specific participants to your study.</a>",
+              participant_help_text: null,
+              is_new: null,
+            },
+            attributes: [
+              {
+                value: whiteListParticipants,
+                name: "white_list",
+                label: "White List",
+                default_value: [],
+              },
+            ],
+            _cls: "web.eligibility.models.CustomWhitelistEligibilityRequirement",
+          },
+        ]
+      : []),
+    ...(blockListParticipants.length > 0
+      ? [
+          {
+            requirement_type: "Block List",
+            query: {
+              question: null,
+              description:
+                "A comma separated list of Prolific IDs of the users you wish to make ineligible for the study.",
+              title: "Custom Blacklist",
+              help_text:
+                "<a href='https://researcher-help.prolific.co/hc/en-gb/articles/360009094374-How-to-prevent-certain-participants-from-accessing-your-study'>Read about how to prevent certain participants from accessing your study.</a>",
+              researcher_help_text:
+                "<a href='https://researcher-help.prolific.co/hc/en-gb/articles/360009094374-How-to-prevent-certain-participants-from-accessing-your-study'>Read about how to prevent certain participants from accessing your study.</a>",
+              participant_help_text: null,
+              is_new: null,
+            },
+            attributes: [
+              {
+                value: blockListParticipants,
+                name: "black_list",
+                label: "Black List",
+                default_value: [],
+              },
+            ],
+            _cls: "web.eligibility.models.CustomBlacklistEligibilityRequirement",
+          },
+        ]
+      : []),
+  ];
+  return req;
+};
 const selectedLocation = {
   "All countries available": "all",
   USA: "usa",
@@ -92,8 +230,8 @@ export const prolificCreateDraft = async (
   completionCode,
   token
 ) => {
-  // const prolificStudyDraftApiUrl = "https://api.prolific.co/api/v1/studies/";
-  const prolificStudyDraftApiUrl = "/.netlify/functions/prolific/studies/";
+  const prolificStudyDraftApiUrl = "https://api.prolific.co/api/v1/studies/";
+  // const prolificStudyDraftApiUrl = "/.netlify/functions/prolific/studies/";
   const hours =
     parseFloat(
       (user.currentExperiment._participantDurationMinutes / 60).toFixed(2)
@@ -138,117 +276,11 @@ export const prolificCreateDraft = async (
       user.currentExperiment._online3DeviceKind?.split(",") ?? [],
     peripheral_requirements:
       user.currentExperiment._online3RequiredServices?.split(",") ?? [],
-    eligibility_requirements: [
-      {
-        id: null,
-        type: "SelectAnswer",
-        attributes: findProlificLanguageAttributes(
-          user.currentExperiment._online5LanguageFirst
-        ),
-        query: {
-          id: "54ac6ea9fdf99b2204feb899",
-          question: "What is your first language?",
-          description: "",
-          title: "First language",
-          help_text: "",
-          participant_help_text: "",
-          researcher_help_text: "",
-          is_new: false,
-          tags: ["onboarding-2", "core-5", "default_export_language"],
-        },
-      },
-      {
-        id: null,
-        type: "MultiSelectAnswer",
-        attributes: findProlificLanguageAttributes(
-          user.currentExperiment._online5LanguageFluent,
-          prolificLangType.FLUENT
-        ),
-        query: {
-          id: "58c6b44ea4dd0a4799361afc",
-          question: "Which of the following languages are you fluent in?",
-          description: "Select the languages that you are fluent in.",
-          title: "Fluent languages",
-          help_text:
-            "<b>Please note:</b> Participants who are fluent in any of the selected languages will be eligible for the study. For example, the study will recruit participants who are fluent in English or German, but not both.",
-          participant_help_text: "",
-          researcher_help_text: "",
-          is_new: false,
-          tags: ["rep_sample_language", "core-13"],
-        },
-      },
-      {
-        id: null,
-        type: "SelectAnswer",
-        attributes: findProlificLocationEligibilityAttributes(
-          user.currentExperiment._online4Location
-        ),
-        query: {
-          id: "54bef0fafdf99b15608c504e",
-          question: "In what country do you currently reside?",
-          description: "",
-          title: "Current Country of Residence",
-          help_text:
-            "Please note that Prolific is currently only available for participants who live in OECD countries. <a href='https://researcher-help.prolific.co/hc/en-gb/articles/360009220833-Who-are-the-people-in-your-participant-pool' target='_blank'>Read more about this</a>",
-          participant_help_text: "",
-          researcher_help_text: "",
-          is_new: false,
-          tags: [
-            "rep_sample_country",
-            "core-7",
-            "default_export_country_of_residence",
-          ],
-        },
-      },
-      {
-        requirement_type: "Allow List",
-        query: {
-          question: null,
-          description:
-            "A comma separated list of Prolific IDs for users you want to participate in the study.",
-          title: "Custom Whitelist",
-          help_text:
-            "ONLY these participants will be eligible for this study. (i.e. longitudinal design). <a href='https://researcher-help.prolific.co/hc/en-gb/articles/360015365674-How-to-invite-specific-participants-to-your-study'>Read about how to invite specific participants to your study.</a>",
-          researcher_help_text:
-            "ONLY these participants will be eligible for this study. (i.e. longitudinal design). <a href='https://researcher-help.prolific.co/hc/en-gb/articles/360015365674-How-to-invite-specific-participants-to-your-study'>Read about how to invite specific participants to your study.</a>",
-          participant_help_text: null,
-          is_new: null,
-        },
-        attributes: [
-          {
-            value: whiteListParticipants,
-            name: "white_list",
-            label: "White List",
-            default_value: [],
-          },
-        ],
-        _cls: "web.eligibility.models.CustomWhitelistEligibilityRequirement",
-      },
-      {
-        requirement_type: "Block List",
-        query: {
-          question: null,
-          description:
-            "A comma separated list of Prolific IDs of the users you wish to make ineligible for the study.",
-          title: "Custom Blacklist",
-          help_text:
-            "<a href='https://researcher-help.prolific.co/hc/en-gb/articles/360009094374-How-to-prevent-certain-participants-from-accessing-your-study'>Read about how to prevent certain participants from accessing your study.</a>",
-          researcher_help_text:
-            "<a href='https://researcher-help.prolific.co/hc/en-gb/articles/360009094374-How-to-prevent-certain-participants-from-accessing-your-study'>Read about how to prevent certain participants from accessing your study.</a>",
-          participant_help_text: null,
-          is_new: null,
-        },
-        attributes: [
-          {
-            value: blockListParticipants,
-            name: "black_list",
-            label: "Black List",
-            default_value: [],
-          },
-        ],
-        _cls: "web.eligibility.models.CustomBlacklistEligibilityRequirement",
-      },
-    ],
+    eligibility_requirements: buildEligibilityRequirements(
+      whiteListParticipants,
+      user,
+      blockListParticipants
+    ),
     project: user.currentExperiment.prolificWorkspaceProjectId ?? undefined,
     selected_location: [
       findProlificLocationAttributes(user.currentExperiment._online4Location),
