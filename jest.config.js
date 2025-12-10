93979b9077a3ab1f5d8d33a78a9a0175935de2e8
khahani <mr.khahani@gmail.com>
2025-12-10 22:51:33 +0330

    add unit tests

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,11 @@ module.exports = {
     "^.+\\.(js|jsx|ts|tsx)$": [
       "babel-jest",
       {
-        presets: ["@babel/preset-env", "@babel/preset-react"],
+        presets: [
+          "@babel/preset-env",
+          "@babel/preset-react",
+          "@babel/preset-typescript",
+        ],
       },
     ],
   },
