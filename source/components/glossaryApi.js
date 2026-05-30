4e26350b8d90149f9de13ffec729a0edfcd57a9d
khahani <mr.khahani@gmail.com>
2026-05-30 12:43:28 +0400

    for threshold: when netlify back-end not runs on localhost then use the production apis

--- source/components/glossaryApi.js
+++ source/components/glossaryApi.js
@@ -2,21 +2,21 @@ import { getEasyEyesBaseUrl } from "../../threshold/components/easyeyesBaseUrl";
 
 export async function fetchGlossaryData() {
   const response = await fetch(
-    `${getEasyEyesBaseUrl()}/.netlify/functions/glossary`,
+    `${await getEasyEyesBaseUrl()}/.netlify/functions/glossary`,
   );
   return response.json();
 }
 
 export async function fetchGlossaryVersion() {
   const response = await fetch(
-    `${getEasyEyesBaseUrl()}/.netlify/functions/glossary?versionOnly=1`,
+    `${await getEasyEyesBaseUrl()}/.netlify/functions/glossary?versionOnly=1`,
   );
   return response.json();
 }
 
 export async function pinGlossaryVersion(username, experimentName) {
   const response = await fetch(
-    `${getEasyEyesBaseUrl()}/.netlify/functions/glossary`,
+    `${await getEasyEyesBaseUrl()}/.netlify/functions/glossary`,
     {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
