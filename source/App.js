486d3f890598f63f52143d7e39cb73097890b1e0
khahani <mr.khahani@gmail.com>
2026-07-27 20:52:19 +0400

    for threshold-scientist: fix: unify compiler latest commit and deploy to UTM format

--- source/App.js
+++ source/App.js
@@ -898,15 +898,17 @@ export default class App extends Component {
                         undefined,
                         {
                           dateStyle: "medium",
+                          timeZone: "UTC",
                         },
                       )}{" "}
                       {new Date(websiteRepoLastCommitDeploy).toLocaleString(
                         undefined,
                         {
                           timeStyle: "short",
+                          timeZone: "UTC",
                         },
                       )}{" "}
-                      {getTimezoneName()}
+                      UTC
                     </a>
                     .{" "}
                   </div>
@@ -954,28 +956,5 @@ export default class App extends Component {
         </Suspense>
       </>
     );
-  }
-}
-
-/* -------------------------------------------------------------------------- */
-
-// https://stackoverflow.com/a/56490104
-function getTimezoneName() {
-  const today = new Date();
-  const short = today.toLocaleDateString(undefined);
-  const full = today.toLocaleDateString(undefined, { timeZoneName: "short" });
-
-  // Trying to remove date from the string in a locale-agnostic way
-  const shortIndex = full.indexOf(short);
-  if (shortIndex >= 0) {
-    const trimmed =
-      full.substring(0, shortIndex) + full.substring(shortIndex + short.length);
-
-    // by this time `trimmed` should be the timezone's name with some punctuation -
-    // trim it from both sides
-    return trimmed.replace(/^[\s,.\-:;]+|[\s,.\-:;]+$/g, "");
-  } else {
-    // in some magic case when short representation of date is not present in the long one, just return the long one as a fallback, since it should contain the timezone's name
-    return full;
   }
 }
